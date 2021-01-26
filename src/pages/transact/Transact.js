1cb495572dd3b5d5631fa58b7aed4a6198eefc30
Faddal Ibrahim <41176161+faddalibrahim@users.noreply.github.com>
2021-01-26 23:14:16 +0000

    transanction page ui on point

--- src/pages/transact/Transact.js
+++ src/pages/transact/Transact.js
@@ -3,6 +3,12 @@ import TextField from "@material-ui/core/TextField";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 import ArrowLeftIcon from "@material-ui/icons/KeyboardArrowLeft";
 import Button from "@material-ui/core/Button";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+
 import "../../scss/transact.scss";
 
 class Transact extends Component {
@@ -15,12 +21,37 @@ class Transact extends Component {
           style={{
             display: "flex",
             alignItems: "center",
+            marginBottom: "2rem",
+            // justifySelf: "start",
+            alignSelf: "flex-start",
           }}
         >
           <ArrowLeftIcon />
           <span>Exit</span>
         </div>
         <form noValidate autoComplete="off">
+          <div>
+            <FormLabel component="legend">Type</FormLabel>
+            <RadioGroup
+              row
+              aria-label="transaction-type"
+              name="transaction-type"
+              value="Income"
+              // onChange={() => alert("its been changed")}
+            >
+              <FormControlLabel
+                value="Income"
+                control={<Radio color="primary" />}
+                label="Income"
+              />
+              <FormControlLabel
+                value="Expenses"
+                control={<Radio color="primary" />}
+                label="Expenses"
+              />
+            </RadioGroup>
+          </div>
+
           <TextField
             id="outlined-password-input"
             label="Amount"
